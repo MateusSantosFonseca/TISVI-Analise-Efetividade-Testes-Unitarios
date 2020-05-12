bbcbaa6975daa6ce6135c93b90f35c667e7e32a6
MateusSantosFonseca <mateus.santos.fsc@gmail.com>
2020-05-12 20:07:34 -0300

    Corrigido código para seguir padrão

--- Scripts/repositorios_downloader.py
+++ Scripts/repositorios_downloader.py
@@ -27,21 +27,21 @@ def baixar_repositorios(path, repositorios, deseja_mostrar_progresso_download):
     except FileExistsError:
         print("\nDiretório:", repos_path, "não foi criado pois já existe.")
 
-    countRepositorionsBaixados = 0
+    count_repos_baixados = 0
     log_downloads = ""
 
     for repositorio in repositorios:
-        countRepositorionsBaixados += 1
+        count_repos_baixados += 1
         splitted_repo = repositorio.split(',')
         nome_repositorio = splitted_repo[1]
-        path_repo_baixado = repos_path + "\\" + "{:04n}".format(countRepositorionsBaixados) + "_" + nome_repositorio
+        path_repo_baixado = repos_path + "\\" + "{:04n}".format(count_repos_baixados) + "_" + nome_repositorio
 
         repos_baixados_list_path.append(path_repo_baixado)
 
         try:
             if not os.path.exists(path_repo_baixado):
                 os.mkdir(path_repo_baixado)
-                print("\nVoce está baixando o repositório: " + nome_repositorio + " ele é o " + str(countRepositorionsBaixados) + "° repositório baixado.")
+                print("\nVoce está baixando o repositório: " + nome_repositorio + " ele é o " + str(count_repos_baixados) + "° repositório baixado.")
                 time.sleep(2)
                 
                 if(deseja_mostrar_progresso_download):
@@ -54,16 +54,16 @@ def baixar_repositorios(path, repositorios, deseja_mostrar_progresso_download):
             
             
             now = datetime.datetime.now()
-            log_downloads += "O repositório: " + nome_repositorio + " foi baixado (ou já estava baixado), ele é o repositorio de número: " + str(countRepositorionsBaixados) + ". Hora do término do download: " + now.strftime("%Y-%m-%d %H:%M:%S") + '\n\n'
+            log_downloads += "O repositório: " + nome_repositorio + " foi baixado (ou já estava baixado), ele é o repositorio de número: " + str(count_repos_baixados) + ". Hora do término do download: " + now.strftime("%Y-%m-%d %H:%M:%S") + '\n\n'
             
         except KeyboardInterrupt:   
             now = datetime.datetime.now()  
-            print ('CTRL + C foi inserido, pulando o repositório: ' + nome_repositorio + " ele era o repositório de número:" + str(countRepositorionsBaixados))
-            log_downloads += "O repositório: " + nome_repositorio + " foi pulado e não foi baixado, ele era o repositorio de número: " + str(countRepositorionsBaixados) + ". Hora do erro: " + now.strftime("%Y-%m-%d %H:%M:%S") + '\n\n'
+            print ('CTRL + C foi inserido, pulando o repositório: ' + nome_repositorio + " ele era o repositório de número:" + str(count_repos_baixados))
+            log_downloads += "O repositório: " + nome_repositorio + " foi pulado e não foi baixado, ele era o repositorio de número: " + str(count_repos_baixados) + ". Hora do erro: " + now.strftime("%Y-%m-%d %H:%M:%S") + '\n\n'
         
         except:
             now = datetime.datetime.now()
-            log_downloads += "O repositório: " + nome_repositorio + " apresentou problemas no download, ele era o repositório de número: " + str(countRepositorionsBaixados) + ". Hora do erro: " + now.strftime("%Y-%m-%d %H:%M:%S") + '\n\n'
+            log_downloads += "O repositório: " + nome_repositorio + " apresentou problemas no download, ele era o repositório de número: " + str(count_repos_baixados) + ". Hora do erro: " + now.strftime("%Y-%m-%d %H:%M:%S") + '\n\n'
 
     print("\nO script de download de repositórios foi finalizado normalmente.")
     print(f"\nLembre-se de deletar os repositórios em: {repos_path}!")
