9b430e0ad4b8549240d77986edd6fcc042a44019
MateusSantosFonseca <mateus.santos.fsc@gmail.com>
2020-05-23 06:35:52 -0300

    Alterada estrutura do código, análise de múltiplas linguagens adicionada

---
+++ Util/linguagens_extensoes_e_nomenclaturas_teste.py
@@ -0,0 +1,9 @@
+linguagem_extensoes_teste = {
+    'JavaScript': ['spec.js', 'karma.js'],
+    'TypeScript': ['spec.js', 'karma.js', 'spec.ts', 'karma.ts'],
+    'Python': ['vazio'],
+    'CSharp': ['vazio'],
+    'Java': ['vazio']
+}
+
+nomenclaturas_comuns_teste = ['test', 'tests']
--- Util/query_repositorios.py
+++ Util/query_repositorios.py
@@ -1,6 +1,8 @@
+#em language tem que ter placeholder
+
 query_repositorios = """
 {
-    search(query:"stars:>100 language:Javascript", type:REPOSITORY, first:10 {placeholder}){
+    search(query:"stars:>100 language:{placeholder_nome_linguagem}", type:REPOSITORY, first:10 {placeholder}){
         nodes{
           ... on Repository
           {
